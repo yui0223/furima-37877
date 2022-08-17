9527a6e11b7e221f3e80d7a4983268c2906b25f2
yui0223 <ranka1113tugumi@gmail.com>
2022-08-17 19:15:38 +0900

    ログイン・ログアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday,        presence: true
+  with_options presence: true do
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+    
+    validates :nickname,        presence: true
+    validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヴー]+\z/u}
+    validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u}
+    validates :birthday,        presence: true
+  end
 end
