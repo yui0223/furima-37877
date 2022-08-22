d132701e812702947a1576b976ae0ac02482aafc
yui0223 <ranka1113tugumi@gmail.com>
2022-08-22 16:42:42 +0900

    モデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   with_options presence: true do
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-    
+
     validates :nickname,        presence: true
-    validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-    validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヴー]+\z/u}
-    validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u}
+    validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
+    validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴー]+\z/u }
     validates :birthday,        presence: true
   end
 end
