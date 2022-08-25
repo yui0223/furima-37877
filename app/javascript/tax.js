211052e4022e7cc81e3f5677e759d78858e014a3
yui0223 <ranka1113tugumi@gmail.com>
2022-08-25 09:45:07 +0900

    必須制限

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -2,11 +2,11 @@ function tax (){
   
   const itemPrice =document.getElementById("item-price"); 
   itemPrice.addEventListener("keyup", () => {
-    const fee = itemPrice.value * 0.1;
+    const fee = Math.floor(itemPrice.value * 0.1);
 
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = `${fee}`;
-    const salesProfit = itemPrice.value - fee;
+    const salesProfit = Math.floor(itemPrice.value - fee);
 
     const profit = document.getElementById("profit");
     profit.innerHTML = `${salesProfit}`;
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,26 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions 
+
   belongs_to :user
+  has_one_attached :image
+
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
+  belongs_to :status
+
+  with_options presence: true do
+    
+    validates :name,             presence: true
+    validates :image,            presence: true
+    validates :description,      presence: true
+    validates :category_id,      presence: true, numericality: { other_than: 1}
+    validates :status_id,        presence: true, numericality: { other_than: 1}
+    validates :shipping_cost_id, presence: true
+    validates :prefecture_id,    presence: true, numericality: { other_than: 1}
+    validates :shipping_date_id, presence: true, numericality: { other_than: 1}
+    validates :price,            presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
 end
