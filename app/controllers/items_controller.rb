9a156ab48354c032f01e3141b591c7676cbd611d
yui0223 <ranka1113tugumi@gmail.com>
2022-08-30 12:10:55 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @item = Item.includes(:user).order('created_at DESC')
   end
@@ -17,9 +18,25 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+      @item = Item.find(params[:id])
+      redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
+  end
+
+  def update
+      @item = Item.find(params[:id])
+    if
+      @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   def show
     @item = Item.find(params[:id])
   end
+
 
   private
 
